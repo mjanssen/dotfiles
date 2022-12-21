736d4d36246e17647d86bd3a14a5527ad1aeeaad
mjanssen <dev.marnix@gmail.com>
2022-12-21 23:21:04 +0100

    initial commit

---
+++ nvim/lua/mjanssen/remap.lua
@@ -0,0 +1,30 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- keep cursor in the middle of screen when jumping up/down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in the middle during search jumps
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep copied items in buffer when pasting with leader
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- allow leader y to yank into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- disable caps Q...
+vim.keymap.set("n", "Q", "<nop>")
+
+-- quickfix list
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace selected word throughout document
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
