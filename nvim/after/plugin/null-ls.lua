71550623c487121f119595b86e50d6f8b466e610
mjanssen <dev.marnix@gmail.com>
2023-01-20 16:49:23 +0100

    Added null-ls config

---
+++ nvim/after/plugin/null-ls.lua
@@ -0,0 +1,11 @@
+local null_ls = require("null-ls");
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.code_actions.eslint,
+        null_ls.builtins.formatting.prettierd,
+    }
+})
+
