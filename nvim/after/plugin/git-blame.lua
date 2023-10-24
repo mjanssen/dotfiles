b061590cec7929647ad5680f86720c61055f2bb1
mjanssen <dev.marnix@gmail.com>
2023-10-24 14:33:51 +0200

    chore: init git blame if config is available

--- nvim/after/plugin/git-blame.lua
+++ nvim/after/plugin/git-blame.lua
@@ -1,6 +1,7 @@
 require("gitblame").setup({
 	enabled = true,
-	gitblame_display_virtual_text = 0,
 })
 
-vim.config.gitblame_display_virtual_text = 0
+if vim.config then
+	vim.config.gitblame_display_virtual_text = 0
+end
