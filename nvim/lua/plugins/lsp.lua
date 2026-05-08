d21b8f9c96658c9969829d2000edd025d0dcf9bd
M. Janssen <mjanssen@mjpolars-2.local>
2026-05-08 10:04:53 +0200

    migrate to snacks

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -138,7 +138,6 @@ return {
 				"bashls",
 				"html",
 				"ruff",
-				"astro",
 			}
 
 			vim.list_extend(ensure_installed, servers_to_install)
@@ -159,8 +158,6 @@ return {
 				lua = true,
 			}
 
-			-- local navic = require("nvim-navic")
-
 			vim.api.nvim_create_autocmd("LspAttach", {
 				callback = function(args)
 					local bufnr = args.buf
@@ -171,40 +168,78 @@ return {
 						settings = {}
 					end
 
-					-- if client.server_capabilities.documentSymbolProvider then
-					-- 	navic.attach(client, bufnr)
-					-- end
-
-					local builtin = require("telescope.builtin")
-
 					vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
-					vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
-					vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
-					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+					vim.keymap.set("n", "gd", function()
+						Snacks.picker.lsp_definitions()
+					end, { buffer = bufnr })
+					vim.keymap.set("n", "gr", function()
+						Snacks.picker.lsp_references()
+					end, { buffer = bufnr })
 
-					vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
-					vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = bufnr })
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr })
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+
+					vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = bufnr })
+					vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = bufnr })
 
 					local filetype = vim.bo[bufnr].filetype
 					if disable_semantic_tokens[filetype] then
 						client.server_capabilities.semanticTokensProvider = nil
 					end
 
-					-- Override server capabilities
 					if settings.server_capabilities then
 						for k, v in pairs(settings.server_capabilities) do
 							if v == vim.NIL then
 								---@diagnostic disable-next-line: cast-local-type
 								v = nil
 							end
-
 							client.server_capabilities[k] = v
 						end
 					end
 				end,
 			})
+
+			--vim.api.nvim_create_autocmd("LspAttach", {
+			--	callback = function(args)
+			--		local bufnr = args.buf
+			--		local client = assert(vim.lsp.get_client_by_id(args.data.client_id), "must have valid client")
+
+			--		local settings = servers[client.name]
+			--		if type(settings) ~= "table" then
+			--			settings = {}
+			--		end
+
+			--		local builtin = require("telescope.builtin")
+
+			--		vim.opt_local.omnifunc = "v:lua.vim.lsp.omnifunc"
+			--		vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+			--		vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+			--		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+			--		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+			--		vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+
+			--		vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, { buffer = 0 })
+			--		vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+
+			--		local filetype = vim.bo[bufnr].filetype
+			--		if disable_semantic_tokens[filetype] then
+			--			client.server_capabilities.semanticTokensProvider = nil
+			--		end
+
+			--		-- Override server capabilities
+			--		if settings.server_capabilities then
+			--			for k, v in pairs(settings.server_capabilities) do
+			--				if v == vim.NIL then
+			--					---@diagnostic disable-next-line: cast-local-type
+			--					v = nil
+			--				end
+
+			--				client.server_capabilities[k] = v
+			--			end
+			--		end
+			--	end,
+			--})
 
 			-- Autoformatting Setup with Conform
 			require("conform").setup({
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				local ensure_installed = {
+					"javascript",
+					"typescript",
+					"tsx",
+					"lua",
+					"rust",
+					"jsdoc",
+					"python",
+					"dockerfile",
+					"toml",
+				}
+
+				local installed = require("nvim-treesitter.config").get_installed()
+				local to_install = vim.iter(ensure_installed)
+					:filter(function(p)
+						return not vim.tbl_contains(installed, p)
+					end)
+					:totable()
+
+				if #to_install > 0 then
+					require("nvim-treesitter").install(to_install)
+				end
+
+				vim.api.nvim_create_autocmd("FileType", {
+					callback = function(args)
+						local lang = vim.treesitter.language.get_lang(args.match)
+						if lang and pcall(vim.treesitter.start, args.buf, lang) then
+							vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+						end
+					end,
+				})
+			end,
+		})
+	end,
+}
