d012947cd75066bd2143b9746650415eda21ec50
mjanssen <mjanssen@mjpolars.local>
2026-05-24 00:54:10 +0200

    add shared Firefox hardening

---
+++ home/dot_config/firefox-hardening/user-overrides.js
@@ -0,0 +1,8 @@
+// Enable userChrome.css / userContent.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+user_pref("browser.startup.page", 3); // restore previous session
+user_pref("browser.search.suggest.enabled", true);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.cookies", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
