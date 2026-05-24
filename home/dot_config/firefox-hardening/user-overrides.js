543e79afebd678d10782cbd12a0d2d3649231511
mjanssen <dev.marnix@gmail.com>
2026-05-25 00:19:38 +0200

    update overrides to match betterfox implementation & update user preferences

--- home/dot_config/firefox-hardening/user-overrides.js
+++ home/dot_config/firefox-hardening/user-overrides.js
@@ -3,9 +3,9 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 user_pref("browser.startup.page", 3); // restore previous session
 user_pref("browser.search.suggest.enabled", true);
+
 user_pref("privacy.clearOnShutdown.history", false);
-user_pref("privacy.clearOnShutdown.cookies", false);
-user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false)
 
 user_pref("privacy.userContext.enabled", true);
 user_pref("privacy.userContext.ui.enabled", true);
@@ -14,3 +14,6 @@ user_pref("sidebar.revamp", true);
 user_pref("sidebar.verticalTabs", true);
 user_pref("sidebar.main.tools", "");
 user_pref("sidebar.visibility", "always-show");
+
+/* END: internal custom pref to test for syntax errors ***/
+user_pref("_user.js.parrot", "Ahoy!");
